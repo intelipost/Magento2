038c352f25f19bc7996cc2ad8ec32095c5337acb
Contardi <thiagocontardi@hotmail.com>
2025-09-21 08:12:03 -0300

    feat: add products to volume array

--- Client/ShipmentOrder/Volume.php
+++ Client/ShipmentOrder/Volume.php
@@ -17,15 +17,16 @@ class Volume
     /**
      * @param $volumes
      * @param $invoice
+     * @param $order
      * @return array
      */
-    public function getInformation($volumes, $invoice)
+    public function getInformation($volumes, $invoice, $order = null)
     {
         $vNumber = 1;
         $vol = json_decode($volumes);
         $volumes = [];
         foreach ($vol as $v) {
-            $volumes[] = $this->getVolume($vNumber, $v, $invoice);
+            $volumes[] = $this->getVolume($vNumber, $v, $invoice, $order);
             $vNumber++;
         }
         return $volumes;
@@ -35,9 +36,10 @@ class Volume
      * @param $vNumber
      * @param $v
      * @param $invoice
+     * @param $order
      * @return \stdClass
      */
-    public function getVolume($vNumber, $v, $invoice)
+    public function getVolume($vNumber, $v, $invoice, $order = null)
     {
         $volume = new \stdClass();
         $volume->shipment_order_volume_number = $vNumber;
@@ -54,6 +56,100 @@ class Volume
             $volume->shipment_order_volume_invoice = $invoice;
         }
 
+        if ($order) {
+            $volume->products = $this->getProductsData($order);
+        }
+
         return $volume;
+    }
+
+    /**
+     * Extract products data from order items for Intelipost API
+     *
+     * @param \Magento\Sales\Model\Order $order
+     * @return array
+     */
+    private function getProductsData($order)
+    {
+        $products = [];
+
+        foreach ($order->getAllVisibleItems() as $item) {
+            $product = $item->getProduct();
+
+            $productData = [
+                'weight' => (float) $item->getWeight() ?: 0.1,
+                'width' => $this->getProductDimension($product, 'width'),
+                'height' => $this->getProductDimension($product, 'height'),
+                'length' => $this->getProductDimension($product, 'length'),
+                'price' => (float) $item->getPrice(),
+                'description' => $this->truncateString($product->getName(), 250),
+                'sku' => $item->getSku(),
+                'quantity' => (int) $item->getQtyOrdered()
+            ];
+
+            $imageUrl = $this->getProductImageUrl($product);
+            if ($imageUrl) {
+                $productData['image_url'] = $this->truncateString($imageUrl, 250);
+            }
+
+            $products[] = $productData;
+        }
+
+        return $products;
+    }
+
+    /**
+     * Get product dimension attribute value
+     *
+     * @param \Magento\Catalog\Model\Product $product
+     * @param string $dimension
+     * @return float
+     */
+    private function getProductDimension($product, $dimension)
+    {
+        $value = 0;
+
+        try {
+            $attributeValue = $product->getData($dimension);
+            if ($attributeValue) {
+                $value = (float) $attributeValue;
+            }
+        } catch (\Exception $e) {
+            $value = 0;
+        }
+
+        return $value ?: 1;
+    }
+
+    /**
+     * Get product image URL
+     *
+     * @param \Magento\Catalog\Model\Product $product
+     * @return string|null
+     */
+    private function getProductImageUrl($product)
+    {
+        try {
+            $imageUrl = $product->getImageUrl();
+            return $imageUrl ?: null;
+        } catch (\Exception $e) {
+            return null;
+        }
+    }
+
+    /**
+     * Truncate string to specified length
+     *
+     * @param string $string
+     * @param int $length
+     * @return string
+     */
+    private function truncateString($string, $length)
+    {
+        if (strlen($string) <= $length) {
+            return $string;
+        }
+
+        return substr($string, 0, $length - 3) . '...';
     }
 }
