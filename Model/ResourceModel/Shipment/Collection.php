a000d4190d38c1145ac57eb9f6767b1add9492e8
Herbert de Oliveira <beto@webjump.ai>
2026-05-18 12:57:21 -0300

    fix: fix typing erros on contructor of some especific classes

--- Model/ResourceModel/Shipment/Collection.php
+++ Model/ResourceModel/Shipment/Collection.php
@@ -43,8 +43,8 @@ class Collection extends AbstractCollection
         FetchStrategyInterface $fetchStrategy,
         ManagerInterface $eventManager,
         StoreManagerInterface $storeManager,
-        AdapterInterface $connection = null,
-        AbstractDb $resource = null
+        AdapterInterface|null $connection = null,
+        AbstractDb|null $resource = null
     )
     {
         $this->_init(
